2a398817598c93292787a4dbf00c09fe41020d6f
Jérémi Savard <jeremi.savard@usherbrooke.ca>
2023-09-03 21:24:46 -0400

    quick sort not recursive working

--- components/sortingVisualizer.jsx
+++ components/sortingVisualizer.jsx
@@ -15,12 +15,46 @@ export default class SortingVisualizer extends React.Component {
         this.resetArray();
     }
 
+    shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
     resetArray(){
         const array = [];
-        for (let i = 0; i < 150; i++){
-            array.push(randomIntFromInterval(5, 730));
+        //for (let i = 0; i < 400; i++){
+        //    array.push(randomIntFromInterval(5, 730));
+        //}
+        for (let i = 0; i < 400; i++) {
+            array.push(i)
         }
+        this.shuffleArray(array);
         this.setState({array});
+        return;
+    }
+
+    quickSort(){
+        let results = [];
+        let temp = 0;
+        let bars = [];
+        results = algo.quickSort(this.state.array)
+        bars = document.getElementsByClassName("sortingVisualizer_arrayBar__Yomyf")
+        
+        for (let i = 0; i < results.length; i++) {
+            setTimeout(() => {
+               if(results[i][0]==results[i][1]){
+                console.log("do nothing")
+               }
+               else{
+                temp = bars[results[i][0]].style.height
+                bars[results[i][0]].style.height = bars[results[i][1]].style.height
+                bars[results[i][1]].style.height = temp
+               }
+               
+            }, i*10);
+        }
     }
 
     bubbleSort(){
@@ -69,15 +103,16 @@ export default class SortingVisualizer extends React.Component {
 
         return (
             <div className={styles.arrayContainer}>
+                <button onClick={() => this.resetArray()}>Generate New Array</button>
                 {array.map((value, idx) => (
                     <div 
                         className={styles.arrayBar} 
                         key={idx}
                         style={{height: `${value}px`}}></div>
-            ))}
-            <button onClick={() => this.resetArray()}>Generate New Array</button>
-            <button onClick={() => this.bubbleSort()}>Quick Sort</button>
-        </div>
+                ))}
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort (not recursive)</button>
+            </div>
       );
     }
 }
--- src/sortingAlgorithms.js
+++ src/sortingAlgorithms.js
@@ -1,22 +1,57 @@
 export function quickSort(arr) {
-    if (arr.length <= 1) {
-      return arr;
+    // Create a stack to store the indices of subarrays to be sorted
+    const stack = [];
+    stack.push(0); // Push the starting index
+    stack.push(arr.length - 1); // Push the ending index
+    let pivotIndex = [0,[]]
+  
+    // Continue until the stack is empty
+    while (stack.length > 0) {
+      // Pop the ending and starting indices
+      const end = stack.pop();
+      const start = stack.pop();
+
+  
+      // Partition the subarray and get the pivot index
+      pivotIndex = partition(arr, start, end, pivotIndex[1]);
+  
+      // If there are elements to the left of the pivot, push their indices to the stack
+      if (pivotIndex[0] - 1 > start) {
+        stack.push(start);
+        stack.push(pivotIndex[0] - 1);
+      }
+  
+      // If there are elements to the right of the pivot, push their indices to the stack
+      if (pivotIndex[0] + 1 < end) {
+        stack.push(pivotIndex[0] + 1);
+        stack.push(end);
+      }
     }
+    return pivotIndex[1]
+  }
   
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
+  function partition(arr, start, end, switchAnimation) {
+    // Choose the pivot element (e.g., the last element)
+    const pivot = arr[end];
+    let i = start - 1;
   
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
+    // Rearrange the elements such that elements smaller than the pivot
+    // are on the left, and elements greater than the pivot are on the right
+    for (let j = start; j < end; j++) {
+      if (arr[j] <= pivot) {
+        i++;
+        // Swap arr[i] and arr[j]
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        switchAnimation.push([i,j])
       }
     }
   
-    //return [...quickSort(left), pivot, ...quickSort(right)];
-    console.log([...quickSort(left), pivot, ...quickSort(right)]);
+    // Swap arr[i+1] (the pivot) and arr[end]
+    [arr[i + 1], arr[end]] = [arr[end], arr[i + 1]];
+    switchAnimation.push([i+1,end])
+  
+    // Return the index of the pivot element
+    return [i + 1, switchAnimation];
   }
 
 export function bubbleSort(arr) {
@@ -38,6 +73,5 @@ for (let i = 0; i < len - 1; i++) {
     }
     }
 }
-console.log(arr)
 return [swap, compare];
 }
