5bc0a44bc597701cef6c3e928084eb566605067b
Jérémi Savard <jeremi.savard@usherbrooke.ca>
2023-09-10 11:34:29 -0400

    add github ci

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const repo = 'Not another sorting visualizer'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}/pages`
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
